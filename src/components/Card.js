c2a54123ea74c4949d5dfa0ef5ecaf8c400f03c1
Vikranth Reddy <gkvikrant@gmail.com>
2021-12-23 16:46:23 +0530

    updated robos

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({name, email, id}) =>{
     // const {name, email, id} = props; we can add this in param 
     return(
         <div className="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
-            <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
+            <img alt="robots" src={`https://robohash.org/${id}?set=set3`} />
                 <div>
                     <h2>{name}</h2>
                     <p>{email}</p>
